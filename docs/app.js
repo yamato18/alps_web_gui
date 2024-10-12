7c8c4dcf78c55676a36211efd9653ef95fe93b5a
yamato18 <yamato151008@gmail.com>
2024-10-13 04:07:17 +0900

    別ライブラリの確認

--- docs/app.js
+++ docs/app.js
@@ -1,39 +1,29 @@
-function ROS(URL) {
-    const ros = new ROSLIB.Ros({
-        url: URL,
-        options: {
-            ros_domain_id: "10"
-        }
-    })
-    
-    ros.on("connection", () => {
-        document.getElementById("status").innerHTML = "successful";
-    });
-    
-    ros.on("error", (error) => {
-        console.log("Error: ", error);
-    });
-    
-    ros.on("close", () => {
-        console.log("Closed");
-    });
-    
-    const image = new ROSLIB.Topic({
-        ros: ros,
-        name: "/image_raw/compressed",
-        messageType: "sensor_msgs/msg/CompressedImage"
-    });
-    
-    image.subscribe((message) => {
-        const data = "data:image/png;base64," + message.data;
-        document.getElementById("ros_image").setAttribute("src", data);
-    });
-}
+const ros = new ROSLIB.Ros({
+    url: "ws://localhost:9090",
+    options: {
+        ros_domain_id: "10"
+    }
+})
 
-ROS("ws://localhost:9090");
+ros.on("connection", () => {
+    document.getElementById("status").innerHTML = "successful";
+});
 
-const btn = document.getElementById("ipButton");
-btn.addEventListener("click", () => {
-    const ip = document.getElementById("ipAddress").value;
-    ROS(ip);
+ros.on("error", (error) => {
+    console.log("Error: ", error);
+});
+
+ros.on("close", () => {
+    console.log("Closed");
+});
+
+const image = new ROSLIB.Topic({
+    ros: ros,
+    name: "/image_raw/compressed",
+    messageType: "sensor_msgs/msg/CompressedImage"
+});
+
+image.subscribe((message) => {
+    const data = "data:image/png;base64," + message.data;
+    document.getElementById("ros_image").setAttribute("src", data);
 });
