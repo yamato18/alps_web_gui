e3c62a9e4fec21e40cd3d7b67bf5f254b723cf20
yamato18 <yamato151008@gmail.com>
2024-10-14 12:53:28 +0900

    検証のためのアップデート

--- docs/app.js
+++ docs/app.js
@@ -30,14 +30,14 @@ const connectROS = () => {
         const status = document.getElementById("status");
         status.textContent = `🔴【ROS接続状況】エラー（${protocol}://${ip}:${port} ID=${ros_domain_id}）`;
         console.log("Error: ", error);
-        document.getElementById("ros_image").setAttribute("src", "NO SIGNAL.png");
+        document.getElementById("ros_image").setAttribute("src", "./NO SIGNAL.png");
     });
     
     ros.on("close", () => {
         const status = document.getElementById("status");
         status.textContent = `🟡【ROS接続状況】未接続（${protocol}://${ip}:${port} ID=${ros_domain_id}）`;
         console.log("Closed");
-        document.getElementById("ros_image").setAttribute("src", "NO SIGNAL.png");
+        document.getElementById("ros_image").setAttribute("src", "./NO SIGNAL.png");
     });
 }
 
