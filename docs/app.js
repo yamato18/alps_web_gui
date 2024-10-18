847d52100eb2ef85c7164b62b162882c52e55f51
yamato18 <yamato151008@gmail.com>
2024-10-18 19:34:58 +0900

    import元を変更

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from "three.module.min.js";
 
 const connectROS = () => {
     // roslib.js
--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // バージョン
-const VERSION = "0.5.6"
+const VERSION = "0.5.7"
 
 // キャッシュ名
 const CACHE_NAME  = `ALPS-Web-GUI-${VERSION}`;
