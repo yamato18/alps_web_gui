05527d058b0cee1fcc3cc0358d10e61422dbb9bf
yamato18 <yamato151008@gmail.com>
2024-10-25 11:09:47 +0900

    three.jsの読み込み方法変更

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // バージョン
-const VERSION = "0.5.12"
+const VERSION = "0.5.13"
 
 // キャッシュ名
 const CACHE_NAME  = `ALPS-Web-GUI-${VERSION}`;
@@ -11,7 +11,7 @@ const CACHE_FILES = [
     "script.js",
     "app.js",
     "roslib.min.js",
-    "three.min.js",
+    "three.module.min.js",
     "tier4-roslibjs-foxglove-0.0.4-bundle.js",
 	"manifest.json",
 	"icons/apple-touch-icon.png",
