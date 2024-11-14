c15da53630720ccf1aa2827cb9e6b29e25eee65a
yamato18 <yamato151008@gmail.com>
2024-11-14 17:01:31 +0900

    通知の改善

--- docs/script.js
+++ docs/script.js
@@ -46,18 +46,25 @@ info.addEventListener("click", () => {
 
 const notice = document.getElementById("notice");
 notice.addEventListener("click", () => {
-    console.log("test");
     
+    // 通知に対応しているか確認
     if (!("Notification" in window)) {
         alert("【ERROR】\nこのブラウザは通知に対応していません。");
+
+    // 通知が既に許可されている場合
     } else if (Notification.permission === "granted") {
         alert("【INFO】\n通知は許可されています。");
-    } else if (Notification.permission !== "denied") {
+
+    // 通知がまだ許可されていない場合
+    } else {
         Notification.requestPermission().then((permission) => {
+
+            // 通知が拒否された場合
             if (permission === "denied") {
                 alert("【ERROR】\n通知は許可されませんでした。");
+
+            // 通知が許可された場合
             } else if (permission === "granted") {
-                alert("成功");
                 navigator.serviceWorker.ready.then((registration) => {
                     registration.showNotification("【INFO】", {
                         body: "通知が許可されました。",
